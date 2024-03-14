9e82cb7f49d06fba814e72a7d1b162e6695b1f65
Sosteem <majhiswastika93@gmail.com>
2024-03-14 07:57:12 -0700

    march 16

---
+++ consoledef/consoledef/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+//there are various methods available in console class
+/*
+ here we are going to take the refer all the console methods which are helpful to take input from the user
+as well as to stream or to write the output on the console
+console.Write("Text here") --> prints and keeps the curser at the same line
+console in console.Write is a class in c# which is pre-defined which gives us to use to its methods one of them is write
+console.Writelie("Text here"); --> prints and keeps the curser at the next line.
+ */
+Console.Write("Hello, World!");
+Console.Write("Its suace in same line?");
+
+Console.WriteLine("Might be in another line nononon hahahaha");
+Console.WriteLine("Might be in another line");
+
+/*
+ console.Read()--> this takes a single input of type string and it returns the ASCII value of that input.
+ console.Readline() --> takes a string  or integer input and returns it as the output value.
+console.ReadKey() -->  takes a single input of type string and it returns the key info
+*/
---
+++ consoles/consoles/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+static void main(string[]args)
+{
+    Console.WriteLine("Enter the value:");
+    string yellow = Console.ReadLine();
+    Console.WriteLine("The entered value is {yellow}");
+}
